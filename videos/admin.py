1ecc57929affdd80903bb5cea60bcb04dc0ae85e
mrawal <imsr1013@gmail.com>
2022-07-07 15:53:36 +0545

    Ordering fields in Admin page

--- videos/admin.py
+++ videos/admin.py
@@ -3,5 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from . import models
 
+
+class MovieAdmin(admin.ModelAdmin):
+    fields = ['release_year', 'title', 'length']
+
+
 admin.site.register(models.Customer)
-admin.site.register(models.Movie)
+admin.site.register(models.Movie,MovieAdmin)
